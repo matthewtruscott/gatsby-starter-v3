58e9f4f5c1906187974e953b3b1f478359c8c328
DESKTOP-3LOG34O\Matth <matthew.truscott@hotmail.co.nz>
2021-10-20 20:08:30 +0100

    Add mixins

--- src/utils/easings.js
+++ src/utils/easings.js
@@ -1,10 +1,11 @@
 export const easeInOut1 = [0.5, 0, 0.5, 1]
 export const easeInOut2 = [0.6, 0, 0.4, 1]
 export const easeInOut3 = [0.7, 0, 0.3, 1]
+export const motionEase = [0.43, 0.13, 0.23, 0.96]
 
 // const spring = {
 //   type: "spring",
 //   stiffness: 250,
 //   damping: 30,
 // }
-export const defaultTransition = { duration: 0.8, ease: easeInOut2 }
+export const defaultTransition = { duration: 0.8, ease: motionEase }
